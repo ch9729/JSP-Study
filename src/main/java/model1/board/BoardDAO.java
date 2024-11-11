46f216c0050b86881e6977a91fb7875964582449
ch9729 <karlll@naver.com>
2024-11-11 12:16:18 +0900

    페이징 있는 게시물 만들기중

--- src/main/java/model1/board/BoardDAO.java
+++ src/main/java/model1/board/BoardDAO.java
@@ -68,6 +68,48 @@ public class BoardDAO extends JDBConnect{
 		}
 		return bbs;
 	}
+	
+	//페이징 된 게시물 목록 가져오기
+	public List<BoardDTO> selectListPage(Map<String, Object> map) {
+		List<BoardDTO> bbs = new Vector<BoardDTO>();	//결과(게시물 목록)를 담을 변수
+		
+		String query = "SELECT * FROM ("
+				+ "    SELECT Tb.*, ROWNUM rNum FROM ("
+				+ "        SELECT * FROM board ";
+		if(map.get("searchWord") != null) {
+			query += " WHERE " + map.get("searchField") + " "
+					+ " LIKE '%" + map.get("searchWord") + "%'";
+		}
+		query += " ORDER BY num DESC"
+				+ "    ) Tb"
+				+ ")"
+				+ "WHERE rNum BETWEEN ? AND ?";
+		
+		try {
+			psmt = con.prepareStatement(query);	//쿼리문 실행
+			psmt.setString(1, map.get("start").toString());
+			psmt.setString(2, map.get("end").toString());
+			
+			rs = psmt.executeQuery();
+			
+			while (rs.next()) {		//결과를 순환하며 한 행(게시물 하나)의 내용을  DTO에 저장
+				BoardDTO dto = new BoardDTO();
+				
+				dto.setNum(rs.getString("num"));
+				dto.setTitle(rs.getString("title"));
+				dto.setContent(rs.getString("content"));
+				dto.setPostdate(rs.getDate("postdate"));
+				dto.setId(rs.getString("id"));
+				dto.setVisitcount(rs.getString("visitcount"));
+				
+				bbs.add(dto); //결과를 저장하여 추가
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return bbs;
+	}
 	//게시글 데이터를 받아서 DB에 추가
 	public int insertWrite(BoardDTO dto) {
 		int result = 0;

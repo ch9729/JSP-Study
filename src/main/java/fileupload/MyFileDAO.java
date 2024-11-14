dec9131ec3e4543cf2e5adbf34a92f7ad6bd5ec0
ch9729 <karlll@naver.com>
2024-11-14 10:03:11 +0900

    파일 업로드 및 다운로드

--- src/main/java/fileupload/MyFileDAO.java
+++ src/main/java/fileupload/MyFileDAO.java
@@ -1,5 +1,8 @@
 package fileupload;
 
+import java.util.List;
+import java.util.Vector;
+
 import common.DBConnPool;
 
 public class MyFileDAO extends DBConnPool{
@@ -10,7 +13,7 @@ public class MyFileDAO extends DBConnPool{
 			String query = "INSERT INTO myfile ( "
 					+ " idx, title, cate, ofile, sfile) "
 					+ "VALUES ( "
-					+ " seq_board_num.nextval, ?, ?, ?, ?, ?)";
+					+ " seq_board_num.nextval, ?, ?, ?, ?)";
 			
 			psmt = con.prepareStatement(query);
 			psmt.setString(1, dto.getTitle());
@@ -24,5 +27,33 @@ public class MyFileDAO extends DBConnPool{
 			e.printStackTrace();
 		}
 		return applyResult;
+	}
+	
+	// 파일 목록을 반환
+	public List<MyFileDTO> myFileList() {
+		List<MyFileDTO> fileList = new Vector<MyFileDTO>();
+		
+		// 쿼리문 작성
+		String query = "SELECT * FROM myfile ORDER BY idx DESC";
+		try {
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(query);
+			
+			while (rs.next()) {
+				MyFileDTO dto = new MyFileDTO();
+				dto.setIdx(rs.getString(1));
+				dto.setTitle(rs.getString(2));
+				dto.setCate(rs.getString(3));
+				dto.setOfile(rs.getString(4));
+				dto.setSfile(rs.getString(5));
+				dto.setPostdate(rs.getString(6));
+				
+				fileList.add(dto);
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return fileList;
 	}
 }
